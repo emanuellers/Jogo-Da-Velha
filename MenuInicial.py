3095fa7fad26eb65910f35642d9376ff11e6a2c8
emanuelle04 <manurs902@gmail.com>
2021-04-07 17:34:29 -0300

    Alteração da disposição dos itens no Menu Incial.

--- MenuInicial.py
+++ MenuInicial.py
@@ -11,21 +11,25 @@ def menuIncial():
     centro = Frame(bg= backgroundColor)#Área principal da aba
 
     global tituloJogo
-    tituloJogo = Label(centro, text="Jogo da Velha 1.0", bg=backgroundColor)
+    tituloJogo = Label(centro, text="Jogo da Velha 1.0", bg=backgroundColor, font=(fontPressStart, 20))
 
     global novoJogoButton
-    novoJogoButton = Button(centro, text="Começar", height=2, width=20,command = lambda:comecar())
+    novoJogoButton = Button(centro, text="Começar", height=2, width=20,command = lambda:comecar(), font=(fontPressStart, 15))
 
     global configuracao
-    configuracao = Button(centro, text="Personalizar", height=2, width=20, command= lambda:personalizar())
+    configuracao = Button(centro, text="Personalizar", height=2, width=20, command= lambda:personalizar(), font=(fontPressStart, 15))
+
+    global github
+    github = Button(centro, text="gitHub", height=2, width=12, font=(fontPressStart, 5), border=0)
 
 
 def criarMenuInicial(boolean):
     menuIncial()
-    centro.grid(row=7, column=10, pady=padyMenuInicialFrame, padx=padxMenuInicialFrame)
-    tituloJogo.grid(row=1, column=2, pady=20, padx=5)
-    novoJogoButton.grid(row=3, column=2, pady=5, padx=5)
-    configuracao.grid(row=5, column=2, pady=5, padx=5)
+    centro.grid(row=7, column=10)
+    tituloJogo.grid(row=1, column=2)
+    novoJogoButton.grid(row=3, column=2)
+    configuracao.grid(row=5, column=2)
+    github.grid(row=10, column=3)
 
 def fecharMenuInicial():
     centro.grid_forget()

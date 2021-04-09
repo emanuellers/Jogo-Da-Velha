8c582a32277944abdc94efebf3d955cdc2a31194
emanuelle04 <manurs902@gmail.com>
2021-04-08 21:46:13 -0300

    Fonte e tamanho dos elementos da aba de configuação definidos.

--- coresTamanhos.py
+++ coresTamanhos.py
@@ -1,41 +1,46 @@
+#Cores e fontes
 backgroundColor = "white"
-fontLabels = "Lohit Tamil"
-fontPressStart = "Press Start 2P Regular"
+fontTexto1 = "MS Reference Sans Serif Regular" #Fonte texto
+fontPressStart = "Press Start 2P Regular"#Fonte título
 corTema = [["Diurno", "#82ABA1", "#e8aeb7"], ["Outono", "#e76f51", "#264653"] , ["Escuro","black", "#fca311"]]
 highlightButtonCores = "#93ff96"
 
 
 #Medidas dos Elementos
-padYItens = 10 #padyItens da menu de configuração
-widthNiveis = 8#width dos itens de dificuldade
+padYItens = 12 #padyItens da menu de configuração
+widthNiveis = 10#width dos itens de dificuldade
 
-widthCoresButton = 10
-widthFormato = 5
-
-widthPartidaButtons_3 = 9
-heightPartidaButtons_3= 4
-
+#Configuracao
+widthCoresButton = 15 #Largura botões Temas
+heightCoresButton = 2 #Altura botões Temas
+padxTemas = 10
+widthFormato = 7
 widthSalvarConfiguracoes = 12
 
-padyMenuInicialFrame = 150
-padxMenuInicialFrame = 210
 
-padxCriarJogoFrame = 30
+#CriarJogo
+widthPartidaButtons_3 = 3 #largura botões do jogo
+heightPartidaButtons_3= 1 #altura botões jogo
 
-#Posicao dos blocos do jogo
+widthLabelCriarJogo =  40
+heighLabelCriarJogo = 4
 
+
+#Posição dos blocos do jogo
 frameJogoRow = 1
 frameJogoMaisUm = 6 
 
 
 #Rostos
 robo = "q|o.o|p" #Máquina
-olhosFechados = ">.<"
-urso = "ʕ•́ᴥ•̀ʔ"
+olhosFechados = "(ㆆ_ㆆ)"
+urso = "ʕ•́ᴥ•̀ʔっ"
 feliz = "(◠‿◠) "
-coracao = "♡"
+coracao = "(─‿‿─)"
 gatinho = "≧◠‿◠≦"
+xis = " (ɔ◔‿◔)ɔ"
+oo = "(ˆ▿ˆc)"
 
-
+#Controle Externo de Pontos do Jogo
 partidas = 0
 pontuacao = 0
